047b75a3945a3839a7c6bde0ba69d894d8b83f84
vasu saini <vasusaini820@gmail.com>
2025-06-02 19:31:25 +0530

    done

--- server/src/routes/workoutRoutes.js
+++ server/src/routes/workoutRoutes.js
@@ -47,42 +47,6 @@ router.post('/', protect, asyncHandler(async (req, res) => {
   res.status(201).json(workout);
 }));
 
-// @desc    Update a workout
-// @route   PUT /api/workouts/:id
-// @access  Private
-router.put('/:id', protect, asyncHandler(async (req, res) => {
-  const workout = await Workout.findOne({ _id: req.params.id, user: req.user._id });
 
-  if (workout) {
-    workout.name = req.body.name || workout.name;
-    workout.type = req.body.type || workout.type;
-    workout.duration = req.body.duration || workout.duration;
-    workout.exercises = req.body.exercises || workout.exercises;
-    workout.date = req.body.date || workout.date;
-    workout.caloriesBurned = req.body.caloriesBurned || workout.caloriesBurned;
-    workout.notes = req.body.notes || workout.notes;
-
-    const updatedWorkout = await workout.save();
-    res.json(updatedWorkout);
-  } else {
-    res.status(404);
-    throw new Error('Workout not found');
-  }
-}));
-
-// @desc    Delete a workout
-// @route   DELETE /api/workouts/:id
-// @access  Private
-router.delete('/:id', protect, asyncHandler(async (req, res) => {
-  const workout = await Workout.findOne({ _id: req.params.id, user: req.user._id });
-
-  if (workout) {
-    await workout.deleteOne();
-    res.json({ message: 'Workout removed' });
-  } else {
-    res.status(404);
-    throw new Error('Workout not found');
-  }
-}));
 
 export default router; 
